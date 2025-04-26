df6def5053630fa48aeec5c0039530a6b35e5238
Александр Сударев <aasudarev@etu.ru>
2025-04-27 00:56:58 +0300

    Пагинация + фикс графиков

--- app/pages/campaign/[id].vue
+++ app/pages/campaign/[id].vue
@@ -1,103 +1,82 @@
 <script setup lang="ts">
+import type {TabsItem} from '@nuxt/ui'
+
 definePageMeta({
   name: 'campaign'
 })
-import type {TabsItem} from '@nuxt/ui'
 
 const categories: Record<string, any> = {
   clicks: {name: 'Переходы', color: '#6aa1fd'}, // Nuxt primary green color
 }
-const route = useRoute()
-
-
-const data = [
-  {
-    date: '1ч',
-    clicks: 2100,
-  },
-  {
-    date: '2ч',
-    clicks: 1200,
-  },
-  {
-    date: '3ч',
-    clicks: 1650,
-  },
-  {
-    date: '4ч',
-    clicks: 1900,
-  },
-  {
-    date: '5ч',
-    clicks: 1400,
-  },
-  {
-    date: '6ч',
-    clicks: 1000,
-  },
-  {
-    date: '7ч',
-    clicks: 1111,
-  },
-  {
-    date: '8ч',
-    clicks: 5100,
-  },
-  {
-    date: '9ч',
-    clicks: 3200,
-  },
-  {
-    date: '10ч',
-    clicks: 1234,
-  },
-  {
-    date: '11ч',
-    clicks: 1450,
-  },
-  {
-    date: '12ч',
-    clicks: 2000,
-  },
-  {
-    date: '13ч',
-    clicks: 1964,
-  },
-  {
-    date: '14ч',
-    clicks: 1945,
-  },
-  {
-    date: '15ч',
-    clicks: 500,
-  },
-  {
-    date: '16ч',
-    clicks: 1200,
-  },
-]
-const filteredData = computed(() => data.filter((item, index) => index % activeTab.value))
-const xFormatter = (i: number) => {
-  if (i % activeTab.value === 0) return data[i]?.date
-  else return ''
-}
-
 const tabs = ref<TabsItem[]>([
   {label: '2ч', value: 2},
   {label: '4ч', value: 4},
   {label: '8ч', value: 8},
   {label: '16ч', value: 16}
 ])
-const activeTab = ref<number>(16)
+const activeTab = ref<number>(2)
+const route = useRoute()
 
 const {data: graphInfo, error} = await useFetch(`http://localhost:8888/api/v1/activity`, {
   method: 'GET',
   query: {
     campaign_id: route.params.id,
-    count_hours: activeTab.value
+    count_hours: 16
   }
 })
-console.log(graphInfo.value)
+const filteredData = computed(() => graphInfo.value.hourly_clicks.filter((item, index: number) => index <= activeTab.value))
+const xFormatter = (i: number) => filteredData.value[i]?.hour
+
+
+interface AreaChartItem {
+  date: string
+  clickCount: number
+  percentage: number
+}
+
+const barCategories: Record<string, BulletLegendItemInterface> = {
+  clickCount: {name: 'Переходы'},
+}
+
+const BarChartData: AreaChartItem[] = [
+  {date: '2024-04-01', clickCount: 222, percentage: 5},
+  {date: '2024-04-02', clickCount: 180, percentage: 2},
+  {date: '2024-04-03', clickCount: 167, percentage: 9},
+  {date: '2024-04-04', clickCount: 172, percentage: 8},
+  {date: '2024-04-05', clickCount: 184, percentage: 2},
+  {date: '2024-04-06', clickCount: 195, percentage: 6},
+  {date: '2024-04-07', clickCount: 198, percentage: 4},
+  {date: '2024-04-08', clickCount: 205, percentage: 9},
+  {date: '2024-04-09', clickCount: 210, percentage: 1},
+  {date: '2024-04-10', clickCount: 219, percentage: 7},
+  {date: '2024-04-11', clickCount: 224, percentage: 3},
+  {date: '2024-04-12', clickCount: 230, percentage: 0},
+]
+
+const xBarFormatter = (i: number): string | number => {
+  if (!BarChartData[i]?.date) {
+    return ''
+  }
+  const date = new Date(BarChartData[i]?.date)
+  return `${date.toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+  })}`
+}
+
+const timeTabs = ref<TabsItem[]>([
+  {label: 'Дни', value: 1},
+  {label: 'Месяцы', value: 2},
+])
+const activeTimeTab = ref<number>(1)
+const formatTabs = ref<TabsItem[]>([
+  {label: '123', value: 1},
+  {label: '%', value: 2},
+])
+const activeFormatTab = ref<number>(1)
+
+
+
 
 </script>
 
@@ -113,19 +92,42 @@ console.log(graphInfo.value)
       <template #default>
         <div>
           <LineChart
-              :data="graphInfo.hourly_clicks"
+              :data="filteredData"
               :height="322"
               :legend-poisition="'Bottom'"
               :categories="categories"
               :y-num-ticks="5"
+              :x-num-ticks="filteredData.length - 1"
               :x-formatter="xFormatter"
               :curve-type="CurveType.MonotoneX"
           />
         </div>
       </template>
-      <template #footer>
-        {{ graphInfo }}
-        {{error}}
+    </UCard>
+    <UCard class="w-full">
+      <template #header>
+        <div class="flex justify-between items-center">
+          <p class="font-semibold">Динамика кликов по кампании</p>
+        </div>
+      </template>
+      <template #default>
+        <div>
+          <BarChart
+              :data="BarChartData"
+              :height="322"
+              :categories="barCategories"
+              :y-axis="['clickCount']"
+              :group-padding="0"
+              :bar-padding="0.2"
+              :yNumTicks="4"
+              :x-formatter="xBarFormatter"
+              :legend-position="LegendPosition.Bottom"
+          />
+          <div class="flex justify-between">
+            <UTabs v-model="activeTimeTab" color="info" :content="false" :items="timeTabs" size="md" class="w-sm"/>
+            <UTabs v-model="activeFormatTab" color="info" :content="false" :items="formatTabs" size="md" class="w-sm"/>
+          </div>
+        </div>
       </template>
     </UCard>
   </UContainer>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,31 +1,49 @@
 <script lang="ts" setup>
 
-const {data: fetchedData} = await useFetch(`http://localhost:8888/api/v1/campaigns`, {
+const page = ref<number>(1)
+const perPage = 10
+
+const {data} = useFetch(`http://localhost:8888/api/v1/campaigns`, {
+  default: () => ({
+    campaigns: [],
+    total: 0
+  }),
+  lazy: true,
+  immediate: true,
   method: 'GET',
   query: {
-    page: 2,
-    per_page: 10
+    page: page,
+    per_page: perPage
   }
 })
-
-
 
 
 </script>
 
 <template>
   <UContainer class="flex flex-col gap-y-6 justify-center max-w-5xl py-8">
-    <UCard v-for="campaign in fetchedData.campaigns" :key="campaign.id">
+    <UCard v-for="campaign in data.campaigns" :key="campaign.id">
       <template #default>
         <div class="flex justify-between items-center">
           <p class="font-semibold">{{ campaign.name }} </p>
-          <UButton :to="{name: 'campaign', params: { id: campaign.id}}"
-                   color="neutral"
-                   variant="ghost"
-                   icon="i-lucide-chevron-right"
+          <UButton
+              :to="{name: 'campaign', params: { id: campaign.id}}"
+              color="neutral"
+              variant="ghost"
+              icon="i-lucide-chevron-right"
           />
         </div>
       </template>
     </UCard>
+    <UPagination
+        v-model:page="page"
+        class="self-center"
+        color="info"
+        variant="neutral"
+        active-color="info"
+        active-variant="solid"
+        show-edges
+        :sibling-count="1"
+        :total="data.total"/>
   </UContainer>
 </template>
